784fd580604c29c4efaafabf652f0db393f36eb8
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-04 22:05:47 +0300

    Add styles for Profile

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,17 @@
-import userData from 'data/user.json'
+import userData from 'data/user.json';
+import statsData from 'data/data.json';
 import Profile from 'components/profile';
+import Statistics from 'components/statistics';
+import css from './app.module.css';
 
 
 const App = () => {
   return (
-    <div>
-      <Profile {...userData}/>
+    <div className={css.wrapper}>
+      <Profile {...userData} />
+      <Statistics title="Upload stats" stats={statsData}/>
     </div>
   )
 };
-
 
 export default App;
