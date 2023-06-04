31730fc09bbfde11388da4f8c1a3012290909df1
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-05 00:49:31 +0300

    Add styles for Statitics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,18 +1,18 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-// import css from './statistics.module.css'
+import css from './statistics.module.css'
 
 const Statistics = ({ title, stats }) => {
 	return (
-		<section className="statistics">
-  <h2 className="title">{title}</h2>
+		<section className={css.statistics}>
+  {title && <h2 className={css.title}>{title}</h2>}
 
-			<ul className="stat-list">
+			<ul className={css['stat-list']}>
 				{stats.map(({id, label, percentage}) => {
 					return (
-						<li key={id} className="item">
-							<span className="label">{label}</span>
-							<span className="percentage">{percentage}</span>
+						<li key={id} className={css.item}>
+							<span className={css.label}>{label}</span>
+							<span className={css.percentage}>{percentage}%</span>
 						</li>
 					)
 				})}
@@ -20,5 +20,14 @@ const Statistics = ({ title, stats }) => {
 		</section>
 	)
 }
+
+Statistics.propTypes = {
+	title: PropTypes.string,
+	stats: PropTypes.exact( {
+		id: PropTypes.string.isRequired,
+		label: PropTypes.string.isRequired,
+		percentage: PropTypes.number.isRequired
+	})
+};
 
 export default Statistics;
