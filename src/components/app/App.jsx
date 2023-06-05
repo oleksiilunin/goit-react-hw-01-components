ad2cf748549e81677e49ca4d78ea862e8b6f0f86
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-06 02:28:48 +0300

    Import and connect components Transaction

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import userData from 'data/user.json';
 import statsData from 'data/data.json';
 import friendsData from 'data/friends.json';
+import transactionsData from 'data/transactions.json';
 
 import Profile from 'components/profile';
 import Statistics from 'components/statistics';
 import FriendList from 'components/friendList';
+import Transactions from 'components/transactions';
 
 import css from './app.module.css';
 
@@ -15,6 +17,7 @@ const App = () => {
       <Profile {...userData} />
       <Statistics title="Upload stats" stats={statsData} />
       <FriendList friends={friendsData} />
+      <Transactions transactions={transactionsData} />
     </div>
   )
 };
