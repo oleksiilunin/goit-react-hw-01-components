528d1e4ae385a590d16995bf48a25d9cdbe854b9
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-05 23:19:28 +0300

    Connect FriendList to App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,11 @@
 import userData from 'data/user.json';
 import statsData from 'data/data.json';
+import friendsData from 'data/friends.json';
+
 import Profile from 'components/profile';
 import Statistics from 'components/statistics';
+import FriendList from 'components/friendList';
+
 import css from './app.module.css';
 
 
@@ -9,7 +13,8 @@ const App = () => {
   return (
     <div className={css.wrapper}>
       <Profile {...userData} />
-      <Statistics title="Upload stats" stats={statsData}/>
+      <Statistics title="Upload stats" stats={statsData} />
+      <FriendList {...friendsData} />
     </div>
   )
 };
