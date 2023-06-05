482269a8cd7b841eb1dd460952e896058cb19131
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-05 23:18:47 +0300

    Create folder and component FriendList

---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,21 @@
+// import PropTypes from 'prop-types';
+
+// import css from './friendList.module.css'
+
+const FriendList = (friends) => {
+	return (
+		<ul class="friend-list">
+		</ul>
+	)
+}
+
+// Friends.propTypes = {
+//   title: PropTypes.string,
+//   stats: PropTypes.arrayOf(PropTypes.exact({
+//     id: PropTypes.string.isRequired,
+//     label: PropTypes.string.isRequired,
+//     percentage: PropTypes.number.isRequired
+//   })).isRequired
+// };
+
+export default FriendList;
