2c284f63ff1516308d9d36a79d6659b471fde91e
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-06 02:15:03 +0300

    Add styles for FriendList

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 
 
-import FriendListItem from "components/friendListItem/FriendListItem";
+import FriendListItem from "components/friendListItem";
 
-// import css from './friendList.module.css'
+import css from './friendList.module.css'
 
 const FriendList = ({friends}) => {
 	return ( 
-		<ul className="friend-list">
-			{friends.map((friend) => <FriendListItem key={friend.id} avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline} />
+		<ul className={css['friend-list']}>
+			{friends.map((friend) => <FriendListItem
+				key={friend.id}
+				avatar={friend.avatar}
+				name={friend.name}
+				isOnline={friend.isOnline} />
 			) }
 		</ul>
 	)
@@ -18,7 +22,7 @@ FriendList.propTypes = {
   friends: PropTypes.arrayOf(PropTypes.exact({
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-		isOnline: PropTypes.number.isRequired,
+		isOnline: PropTypes.bool.isRequired,
 		id: PropTypes.number.isRequired
   })).isRequired
 };
