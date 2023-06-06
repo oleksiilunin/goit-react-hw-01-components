b2fb07e839a9af46e25d3271ed981703328175e6
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-06 04:12:33 +0300

    Add styles for Transactions

--- src/components/transactions/Transactions.jsx
+++ src/components/transactions/Transactions.jsx
@@ -1,5 +1,29 @@
-const Transactions = () => {
-	return null
-}
+import css from './transactions.module.css';
+
+const Transactions = ({ transactions }) => {
+  return (
+    <table className={css['transaction-history']}>
+      <thead>
+        <tr>
+          <th className={css.type}>Type</th>
+          <th className={css.amount}>Amount</th>
+          <th className={css.currency}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td className={css.type}>{type}</td>
+              <td className={css.amount}>{amount}</td>
+              <td className={css.currency}>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default Transactions;
