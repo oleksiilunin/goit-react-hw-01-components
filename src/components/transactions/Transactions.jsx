38fb968c6ff390598c007dc4e193e861820de781
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-06 04:19:39 +0300

    Del unused classes

--- src/components/transactions/Transactions.jsx
+++ src/components/transactions/Transactions.jsx
@@ -5,9 +5,9 @@ const Transactions = ({ transactions }) => {
     <table className={css['transaction-history']}>
       <thead>
         <tr>
-          <th className={css.type}>Type</th>
-          <th className={css.amount}>Amount</th>
-          <th className={css.currency}>Currency</th>
+          <th >Type</th>
+          <th >Amount</th>
+          <th >Currency</th>
         </tr>
       </thead>
 
@@ -15,9 +15,9 @@ const Transactions = ({ transactions }) => {
         {transactions.map(({ id, type, amount, currency }) => {
           return (
             <tr key={id}>
-              <td className={css.type}>{type}</td>
-              <td className={css.amount}>{amount}</td>
-              <td className={css.currency}>{currency}</td>
+              <td >{type}</td>
+              <td >{amount}</td>
+              <td >{currency}</td>
             </tr>
           );
         })}
